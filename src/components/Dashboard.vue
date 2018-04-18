cf3892b2d9a12db40f91cd9936e1f4cb28acad15
polawat phetra <pphetra@gmail.com>
2018-04-18 22:36:14 +0300

    Set map center to uganda.

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="map">
+    <v-map style="height:100%" :zoom="8" :center="mapCenter">
+      <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+    </v-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    'v-map': LMap,
+    'v-tilelayer': LTileLayer
+  },
+  data: function () {
+    return {
+      mapCenter: [1.3671058, 30.0588381]
+    }
+  }
+}
+</script>
+
+<style>
+#map {
+  height: 100%;
+  width: 100%;
+}
+</style>
